7f42611a8aab88729ff9c7b0954554e6ae8e47f3
Walter White <walterwhiteheisenberg84@outlook.fr>
2021-01-07 14:26:34 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version "bodacious"
+
+game {"gta5"}
+
+author "French Boomer"
+
+description "Script lua permettant une annonce lors des tempete en RP puis l'exclusion totale de tous les joueurs presents"
+
+version "1.0.0"
+
+shared_script "Config/*.lua"
+
+server_scripts "Serveur/*.lua"
+
+client_scripts "Client/*.lua"
